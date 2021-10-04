13cea44c7d5dedd75966c7b86f0f9bb5e1cc4121
Jason Mohabir <jmohabir30@stuy.edu>
2021-10-04 18:39:35 -0400

    Unit Setup

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,7 @@
+public class Greet{
+  public static void main(String[] args){
+    System.out.println("Why, hello there, Foo. How do you do?");
+    System.out.println("Why, hello there, Moo. How do you do?");
+    System.out.println("Why, hello there, Boo. How do you do?");
+  }
+}
