804bbd3e2fe4173824c5d788cd1bfb93cb166511
Jason Mohabir <jmohabir30@stuy.edu>
2021-10-04 18:52:17 -0400

    Unit Setup

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,5 @@
+public class BigSib{
+  public static String greet(String name){
+    return "Why, hello there, " + name + ". How do you do?";
+  }
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,7 @@
+public class Greet{
+  public static void main(String[] args) {
+    System.out.println(BigSib.greet("Dean"));
+    System.out.println(BigSib.greet("Gene"));
+    System.out.println(BigSib.greet("Ween"));
+  }
+}
