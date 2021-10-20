3feb2ec6eb6ccb2e035e00e87fd248ee4b57c835
jus910 <58864969+jus910@users.noreply.github.com>
2021-10-20 06:07:19 -0400

    Update Teller.java

--- 20_whitehat/Teller.java
+++ 20_whitehat/Teller.java
@@ -3,8 +3,16 @@ APCS
 HW20: External Audit
 2021 10 19
 QCC:
-- What does it really mean to break an object? Does it involve run-time errors? 
+- What does it really mean to break an object? 
+- Does it involve run-time or compile errors? 
+- Should we focus on making the program trip us? 
 Disco:
+- Compile error when int is out of range (integer number too large)
+- When placing zeroes in front of the deposit and withdraw int values, they don't seem to work correctly 
+	- when we deposit 01000, the balance should be 1000, but is instead 512
+	- when we deposit 001000, the balance is still 512
+	- when we deposit 001101, the balance is 577
+- WHen we place zeroes in front of the int value, java seems to read the numbers in base-8
 */
 
 public class Teller{
@@ -14,22 +22,35 @@ public class Teller{
 	BankAccount test = new BankAccount();
 	test.setName("Tater Tots");
 	test.setPasswd("Taters123456");
-	test.setPin((short) 12345);
-	test.AcctNum(123456789);
+	test.setPin((short) 1234);
+	test.setAcctNum(123456789);
 	test.setBalance(60000000.00);
-	test.deposit(40000000.00);
-	test.withdraw(10000000000000000);
+	test.deposit(4000.00);
+	test.withdraw(1000);
 	System.out.println(test.toString());
 
 	// Breaking The Bank
-	BankAccount test = new BankAccount();
-	test.setName("Tater Tots");
-	test.setPasswd("Taters123456");
-	test.setPin((short) 12345);
-	test.AcctNum(123456789);
-	test.setBalance(600000000000000000000000000000000000000000000.00);
-	test.deposit(400000000000000000000000000000);
-	test.withdraw(10000000000000000000000000000000);
-	System.out.println(test.toString());
+	BankAccount hack = new BankAccount();
+	hack.setName("Tater Tots");
+	hack.setPasswd("Taters123456");
+	hack.setPin((short) 1234);
+	hack.setAcctNum(100000002);
+	hack.setBalance(0);
+	hack.deposit(001101);
+	hack.withdraw(0);
+	System.out.println(hack.toString());
+
+	// Breaking The Bank error
+	/*
+	BankAccount hack2 = new BankAccount();
+	hack2.setName("Tater Tots");
+	hack2.setPasswd("Taters123456");
+	hack2.setPin((short) 1234);
+	hack2.setAcctNum(123456789);
+	hack2.setBalance(600000000000000000000000000000000000000000000.00);
+	hack2.deposit(400000000000000000000000000000);
+	hack2.withdraw(10000000000000000000000000000000);
+	System.out.println(hack2.toString());
+	*/
 	}
 }
