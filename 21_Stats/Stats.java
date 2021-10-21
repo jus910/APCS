b60fddab7dcd7e9cdd13e77f777d84520399eab8
jus910 <58864969+jus910@users.noreply.github.com>
2021-10-20 22:00:43 -0400

    Create Stats.java

---
+++ 21_Stats/Stats.java
@@ -0,0 +1,60 @@
+/**
+   Clyde "Thluffy" Sinclair
+   APCS
+   HW20 --
+   2021-10-21
+   TASK:
+   Implement each method without typecasting.
+   xtra: harmonic mean is the reciprocal of the
+   arithmetic mean of the reciprocals
+ **/
+
+public class Stats {
+
+  public static int mean(int a, int b) {
+
+  }
+
+  public static double mean(double a, double b) {
+
+  }
+
+  public static int max(int a, int b) {
+
+  }
+
+  public static double max(double a, double b) {
+
+  }
+
+  public static int geoMean(int a, int b) {
+
+  }
+
+  public static double geoMean(double a, double b) {
+
+  }
+
+  public static int max(int a, int b, int c) {
+
+  }
+
+  public static double max(double a, double b, double c) {
+
+  }
+
+  public static int geoMean(int a, int b, int c) {
+
+  }
+
+  public static double geoMean(double a, double b, double c) {
+
+  }
+
+
+  //main method for testing functionality
+  public static void main( String[] args ) {
+
+  }
+
+}//end class
