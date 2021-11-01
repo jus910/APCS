08492c803138a7e58f93bc35d9ebea2da5a831ec
jus910 <58864969+jus910@users.noreply.github.com>
2021-10-31 21:34:25 -0400

    Update catDog.java

--- cb/String-2/catDog.java
+++ cb/String-2/catDog.java
@@ -1,3 +1,14 @@
 public class catDog{
-  
+  public boolean catDog(String str) {
+  int cat=0;
+  int dog=0;
+  for(int i=0;i<str.length()-2;i=i+1){
+    if ((str.substring(i,i+3)).equals("cat")){
+      cat++;
+    } else if((str.substring(i,i+3)).equals("dog")){
+      dog++;
+    }
+  }
+  return cat==dog;
+}
 }
