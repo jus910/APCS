fac3a6deffcaa747cbb6496c2e819b73cbf9a7fd
Jason Mohabir <jmohabir30@stuy.edu>
2021-10-31 21:31:16 -0400

    ok

--- cb/String-2/countHi.java
+++ cb/String-2/countHi.java
@@ -1,3 +1,16 @@
 public class countHi{
-  
+  public int countHi(String str) {
+  int hi=0;
+  for(int i=0;i<str.length()-1;i=i+1){
+    if ((str.substring(i,i+2)).equals("hi")){
+      hi++;
+    }
+  }
+  return hi;
+}
+
+  public static void main(String[] args){
+    countHi james = new countHi();
+    System.out.println(james.countHi("hihi"));
+  }
 }
