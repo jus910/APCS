932ec718edc004fc4d4db5720495ab98c4c2eb82
jus910 <58864969+jus910@users.noreply.github.com>
2021-10-31 21:37:18 -0400

    Update countCode.java

--- cb/String-2/countCode.java
+++ cb/String-2/countCode.java
@@ -1,3 +1,13 @@
 public class countCode{
-  
+  public int countCode(String str) {
+  int code=0;
+  for(int i=0;i<str.length()-3;i=i+1){
+    if ((str.substring(i,i+2)).equals("co")){
+      if ((str.substring(i+3,i+4)).equals("e")){
+        code++;
+      }
+    }
+  }
+  return code;
+}
 }
