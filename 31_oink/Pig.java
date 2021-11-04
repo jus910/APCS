70f997d9b331b907833a6116ee3dc9e1e2389b0b
Jason Mohabir <jmohabir30@stuy.edu>
2021-11-03 20:43:37 -0400

    update

--- 31_oink/Pig.java
+++ 31_oink/Pig.java
@@ -35,7 +35,7 @@ public class Pig
     post: hasA("cat", "a") -> true
     hasA("cat", "p")       -> false
     =====================================*/
-  public static boolean hasA( String w, String letter ) 
+  public static boolean hasA( String w, String letter )
   {
     return w.contains(letter);
   }//end hasA()
@@ -45,8 +45,9 @@ public class Pig
     boolean isAVowel(String) -- tells whether a letter is a vowel
     precondition: letter.length() == 1
     =====================================*/
-  public static boolean isAVowel( String letter ) 
+  public static boolean isAVowel( String letter )
   {
+    return hasA(VOWELS, letter);
     /* YOUR IMPLEMENTATION HERE */
   }
 
@@ -56,8 +57,15 @@ public class Pig
     pre:  w != null
     post: countVowels("meatball") -> 3
     =====================================*/
-  public static int countVowels( String w ) 
+  public static int countVowels( String w )
   {
+    int counter=0;
+    for (int i=0; i<w.length();i++){
+      if (isAVowel(w.substring(i,i+1))){
+        counter++;
+      }
+    }
+    return counter;
     /* YOUR IMPLEMENTATION HERE */
   }
 
@@ -68,27 +76,48 @@ public class Pig
     post: hasAVowel("cat") -> true
     hasAVowel("zzz")       -> false
     =====================================*/
-  public static boolean hasAVowel( String w ) 
+  public static boolean hasAVowel( String w )
   {
-    /* YOUR IMPLEMENTATION HERE */
+    for (int i=0; i<w.length();i++){
+      if (isAVowel(w.substring(i,i+1))){
+        return true;
+  		}
+    }
+    return false;
   }
-
 
   /*=====================================
     String allVowels(String) -- returns vowels in a String
     pre:  w != null
     post: allVowels("meatball") -> "eaa"
     =====================================*/
-  public static String allVowels( String w ) 
+  public static String allVowels( String w )
   {
+    String output = "";
+    for (int i=0; i<w.length();i++){
+      if (isAVowel(w.substring(i,i+1))){
+        output = output + w.substring(i,i+1);
+  		}
+    }
+    return output;
     /* YOUR IMPLEMENTATION HERE */
   }
 
 
-  public static void main( String[] args ) 
+  public static void main( String[] args )
   {
-    hasA("cat", "a")
-    hasA("cat", "p")
+    System.out.println(hasA("cat", "a")); //returns true
+    System.out.println(hasA("cat", "p")); //returns false
+    System.out.println("//");
+    System.out.println(isAVowel("a")); //returns true
+    System.out.println(isAVowel("z")); //returns false
+    System.out.println("//");
+    System.out.println(countVowels("meatball")); //returns 3
+    System.out.println("//");
+    System.out.println(hasAVowel("cat")); //returns true
+    System.out.println(hasAVowel("zzz")); //returns false
+    System.out.println("//");
+    System.out.println(allVowels("meatball")); //returns "eaa"
   }//end main()
 
 }//end class Pig
