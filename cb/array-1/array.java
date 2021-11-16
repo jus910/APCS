30ef16d47aa55d539f9982641a51cc3f2d6524fa
jus910 <58864969+jus910@users.noreply.github.com>
2021-11-16 13:37:27 -0500

    Update array.java

--- cb/array-1/array.java
+++ cb/array-1/array.java
@@ -1,3 +1,7 @@
 public boolean firstLast6(int[] nums) {
   return nums[0]==6 || nums[nums.length-1]==6;
 }
+
+public boolean sameFirstLast(int[] nums) {
+  return nums.length>0 && nums[0]==nums[nums.length-1];
+}
