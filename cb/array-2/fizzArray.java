d9d155dd9a6822a13d96dbcb0642dd67fc56025c
jus910 <58864969+jus910@users.noreply.github.com>
2021-11-16 19:16:13 -0500

    Create fizzArray.java

---
+++ cb/array-2/fizzArray.java
@@ -0,0 +1,9 @@
+public class fizzArray{
+  public int[] fizzArray(int n) {
+    int[] array = new int[n];
+    for(int x = 0; x < array.length; x++) {
+              array[x] = x;
+    }
+    return array;
+  }
+}
