e6d8f49c45194b422181874cfb1b616cb7e38674
jus910 <58864969+jus910@users.noreply.github.com>
2021-11-16 19:14:10 -0500

    Create has22.java

---
+++ cb/array-2/has22.java
@@ -0,0 +1,10 @@
+public class has22{
+  public boolean has22(int[] nums) {
+    for(int i = 0; i<nums.length-1; i++){
+      if (nums[i] == 2 && nums[i+1] == 2) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
