8a713251d450c2a5e68fc267b08d6aa0e67bcd1f
jus910 <58864969+jus910@users.noreply.github.com>
2021-11-16 19:05:00 -0500

    Create reverse3.java

---
+++ cb/array-1/reverse3.java
@@ -0,0 +1,9 @@
+public class reverse3{
+  public int[] reverse3(int[] nums) {
+    int[] output = new int[3];
+    output[0] = nums[2];
+    output[1] = nums[1];
+    output[2] = nums[0];
+    return output;
+  }
+}
