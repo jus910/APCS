d93659c3d2459091c26a17f5ee1ee69b2d5b59f4
jus910 <58864969+jus910@users.noreply.github.com>
2021-11-16 19:08:51 -0500

    Delete array.java

--- cb/array-1/array.java
+++
@@ -1,7 +0,0 @@
-public boolean firstLast6(int[] nums) {
-  return nums[0]==6 || nums[nums.length-1]==6;
-}
-
-public boolean sameFirstLast(int[] nums) {
-  return nums.length>0 && nums[0]==nums[nums.length-1];
-}
