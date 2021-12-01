dc56a9d77190dae307805ec7f724ac8c65dcd56a
Justin <jmohabir30@stuy.edu>
2021-12-01 13:24:25 -0500

    ok

---
+++ 41_rat/Rational.java
@@ -0,0 +1,20 @@
+public class Rational{
+	int numerator;
+	int denominator;
+	public Rational(){
+		numerator=0;
+		denominator=1;
+	}
+	public Rational(int n, int d){
+		this();
+		if (d!=0){
+			
+		} else{
+			numerator=n;
+			denominator=d;
+		}
+	} 
+	public static void main(String[] args){
+		System.out.println("Hi");
+	}
+}
