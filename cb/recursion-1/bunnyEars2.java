7944adad85c4b995e0295e7d760649e0cd2b40d6
jus910 <58864969+jus910@users.noreply.github.com>
2022-02-14 18:36:15 -0500

    Create bunnyEars2.java

---
+++ cb/recursion-1/bunnyEars2.java
@@ -0,0 +1,11 @@
+public class bunnyEars2{
+  public int bunnyEars2(int bunnies) {
+    if(bunnies==0){
+      return 0;
+    } else if((bunnies%2)==0){
+      return bunnyEars2(bunnies-1)+3;
+    } else{
+      return bunnyEars2(bunnies-1)+2;
+    }
+  }
+}
