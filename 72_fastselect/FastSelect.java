467d1dae40d578893748da48909edc21174b966a
jus910 <58864969+jus910@users.noreply.github.com>
2022-03-08 21:24:56 -0500

    Update FastSelect.java

--- 72_fastselect/FastSelect.java
+++ 72_fastselect/FastSelect.java
@@ -8,7 +8,7 @@ Algo; Partition the list y times and return the yth Value
 BEST CASE SCENARIO: The list is sorted in descending order. The run time is O(n^2) because it must partition n times, and the partition is O(n)
 WORST CASE SCENARIO: The list is sorted in ascending order. The run time is O(n^2) because it must partition n times, and the partition is O(n)
 Disco: If you partition the same list for long enough it becomes sorted
-QCC: I
+QCC: 
   Is there a more efficient way?
   How do we address multiple variables when it comes to time complexity?
 ***/
